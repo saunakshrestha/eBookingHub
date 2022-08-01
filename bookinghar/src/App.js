19661f8f609ad9a327c0afa549a5ed9cbac918b2
saunakshrestha <sunk.stha@gmail.com>
2022-08-01 23:10:53 +0545

    Initial for today

--- bookinghar/src/App.js
+++ bookinghar/src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import List from './pages/list/List'
+import Hotel from './pages/hotel/Hotel'
+
+
 function App() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotels' element={<List />} />
+        <Route path='/hotels/:id' element={<Hotel />} />
+      </Routes>
+      <h1>App</h1>
+    </div>
+  )
 }
 
 export default App
