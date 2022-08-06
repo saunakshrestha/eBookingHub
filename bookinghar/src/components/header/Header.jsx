538740eeac53ab0a00e7ce83a5a93ee96df97d52
saunakshrestha <sunk.stha@gmail.com>
2022-08-06 14:41:19 +0545

    Header design

--- bookinghar/src/components/header/Header.jsx
+++ bookinghar/src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import './header.css'
 import {
   faBed,
-  //   faCalendarDays,
-  //   faPerson,
-  //   faPlane,
+  faCalendarDays,
+  faPerson,
+  
 } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
@@ -14,9 +14,37 @@ const Header = () => {
         <div className='headerList'>
           <div className='headerListItem'>
             <FontAwesomeIcon icon={faBed} />
-            <span>Stays</span>
+            <span>Hotels</span>
           </div>
         </div>
+        <>
+          <h1 className='headerTitle'>Looking for hotels? Explore Here!</h1>
+          <p className='headerDesc'>
+            Unlock discounts of 20% ✅ with your first booking.😍
+          </p>
+          <button className='headerBtn'>Sign in / Register</button>
+          <div className='headerSearch'>
+            <div className='headerSearchItem'>
+              <FontAwesomeIcon icon={faBed} className='headerIcon' />
+              <input
+                type='text'
+                placeholder='Where are you going?'
+                className='headerSearchInput'
+              />
+            </div>
+            <div className='headerSearchItem'>
+              <FontAwesomeIcon icon={faCalendarDays} className='headerIcon' />
+              <span className='headerSearchText'>Date</span>
+            </div>
+            <div className='headerSearchItem'>
+              <FontAwesomeIcon icon={faPerson} className='headerIcon' />
+              <span className='headerSearchText'>Children Parents</span>
+            </div>
+            <div className='headerSearchItem'>
+              <span className='headerSearchBtn'>Search</span>
+            </div>
+          </div>
+        </>
       </div>
     </div>
   )
