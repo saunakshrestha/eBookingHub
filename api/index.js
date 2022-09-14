15342768b1234135870bb3e56284f13bf5047222
Saunak Shrestha <sunk.stha@gmail.com>
2022-09-14 16:19:47 +0545

    Starting Middlewares study section

--- api/index.js
+++ api/index.js
@@ -28,6 +28,8 @@ app.get("/",(req,res) => {
 
 
 //middlewares
+app.use(express.json());
+
 app.use('/api/auth', authRoute)
 app.use('/api/users', usersRoute)
 app.use('/api/hotels', hotelsRoute)
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -5,26 +5,64 @@ import Hotel from '../models/Hotel.js'
 const router = express.Router()
 
 //CREATE
-router.post('/', async(req,res)=>{
-    const newHotel = new Hotel(req.body)
-    try{
-
-        const savedHotel = await newHotel.save()
-        res.status(200).json(savedHotel)
-
-    }catch(err){
-        res.status(500).json(err)
-    }
+router.post('/', async (req, res) => {
+  const newHotel = new Hotel(req.body)
+  try {
+    const savedHotel = await newHotel.save()
+    res.status(200).json(savedHotel)
+  } catch (err) {
+    res.status(500).json(err)
+  }
 })
 
 //UPDATE
-// router.put('/:id', verifyAdmin, updateHotel)
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    )
+    res.status(200).json(updatedHotel)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
 //DELETE
-// router.delete('/:id', verifyAdmin, deleteHotel)
+router.delete('/:id', async (req, res) => {
+  try {
+    const updatedHotel = await Hotel.findByIdAndDelete(
+      req.params.id
+    )
+    res.status(200).json("Hotel has been deleted.")
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 //GET
 
-// router.get('/find/:id', getHotel)
-//GET ALL
+router.get('/:id', async (req, res) => {
+  try {
+    const hotel = await Hotel.findById(
+      req.params.id
+    )
+    res.status(200).json(hotel)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
+
+//GETALL
+
+router.get('/', async (req, res) => {
+  try {
+    const hotels = await Hotel.find()
+    res.status(200).json(hotels)
+  } catch (err) {
+    res.status(500).json(err)
+  }
+})
 
 // router.get('/', getHotels)
 // router.get('/countByCity', countByCity)
